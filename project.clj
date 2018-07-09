05f686baa94701e498ace695f2c95fe444dbce7e
Reid 'arrdem' McKenzie <me@arrdem.com>
2018-07-09 15:37:08 -0700

    Kill off midje

--- project.clj
+++ project.clj
@@ -4,7 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   
-  :dependencies [[org.clojure/clojure "1.9.0"]]
-  :profiles {:dev {:dependencies [[midje "1.9.1"]]
-                   :plugins [[lein-midje "3.2.1"]]
-                   :aliases {"test" ["midje"]}}})
+  :dependencies [[org.clojure/clojure "1.9.0"]])
